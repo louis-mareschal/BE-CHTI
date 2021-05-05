779185b082637d32b8788a70405839ea3642e2e7
raphael-brgs <raphael.bourgeois972@gmail.com>
2021-05-05 18:13:56 +0200

    debut de la dft, rien d'amorcer

--- ASM/dft/PjtKEIL_StepDFT/Src/principal.c
+++ ASM/dft/PjtKEIL_StepDFT/Src/principal.c
@@ -1,8 +1,6 @@
-
-
 #include "DriverJeuLaser.h"
 
-
+int DFT_ModuleAuCarre( short int * Signal64ech, char k) ;
 
 int main(void)
 {
@@ -23,6 +21,7 @@ CLOCK_Configure();
 	
 while	(1)
 	{
+		
 	}
 }
 
